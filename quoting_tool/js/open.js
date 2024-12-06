39eadcb010e48584e5ca87f9514a73809b3e8fe0
RobertHauta <97628834+RobertHauta@users.noreply.github.com>
2024-12-06 13:13:57 -0700

    Update open.js

--- quoting_tool/js/open.js
+++ quoting_tool/js/open.js
@@ -1,2 +1,14 @@
-var windowOptions = { height: 800, width: 800 };
-Xrm.Navigation.openWebResource("new_/quoting_tool/html/retrieve_test.html",windowOptions);
+var windowOptions = "height=800,width=800";
+var url = "new_/quoting_tool/html/product_catalogue.html";
+
+// Open the web resource
+Xrm.Navigation.openWebResource(url, windowOptions);
+
+function refreshPage() {
+    
+console.log(window.parent.location.href);
+    // Refresh the page by reloading the current browser window
+    window.parent.location.replace(window.parent.location.href);
+}
+refreshPage();
+//OpenQuote!d1a7fd26e9e4461881e8a88e7ac4d3f1
