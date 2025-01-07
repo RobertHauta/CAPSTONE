6b3be4bb272741221e855af01ef0b4709d3d5b15
RobertHauta <97628834+RobertHauta@users.noreply.github.com>
2025-01-07 00:08:58 -0700

    Update open.js

--- quoting_tool/js/open.js
+++ quoting_tool/js/open.js
@@ -1,8 +1,36 @@
+console.log("open");
 var windowOptions = "height=800,width=800";
-var url = "new_/quoting_tool/html/product_catalogue.html";
+var url = "new_/quoting_tool/html/quote_page.html";
+var formContext = Xrm.Page;
+getFormData(formContext);
 
 // Open the web resource
-Xrm.Navigation.openWebResource(url, windowOptions);
+if(formContext.data.getIsDirty()){
+    console.log("dirty");
+    alert("Unsaved Changes: Please save your quote before opening the Quoting Tool.");
+}
+else{
+    console.log("clean");
+    Xrm.Navigation.openWebResource(url);
+    refreshPage();
+}
+
+function getFormData(formContext) {
+    console.log(formContext);
+    // Retrieve the value of the 'quotenumber' field
+    var ids = {
+        quoteNumber: formContext.getAttribute("quotenumber").getValue(),
+        revisionNumber: formContext.getAttribute("revisionnumber").getValue()
+    };
+    
+    if (!ids) {
+        console.error("missing!");
+    }
+    else{
+        var stringified = JSON.stringify(ids);
+        sessionStorage.setItem("ID", stringified);
+    }
+}
 
 function refreshPage() {
     
@@ -10,5 +38,5 @@ console.log(window.parent.location.href);
     // Refresh the page by reloading the current browser window
     window.parent.location.replace(window.parent.location.href);
 }
-refreshPage();
+
 //OpenQuote!d1a7fd26e9e4461881e8a88e7ac4d3f1
